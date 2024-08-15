47e4c88dfafae73ad32659fa13d3e17472c82989
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2024-08-15 20:11:35 -0300

    Update file_system.gd

--- util/file_system.gd
+++ util/file_system.gd
@@ -59,6 +59,8 @@ static func get_files_in_folder(path: String, recursive: bool = false) -> Array[
 	dir.list_dir_begin()
 	var file_name := dir.get_next()
 	while file_name != "":
+		if file_name.ends_with(".import"):
+			continue
 		var file_path = path + file_name
 		if dir.current_is_dir():
 			if recursive:
